52fd23124dffabd52a1f598eac16164dfc5d6c1b
Jude Tijani <teejayjude@gmail.com>
2023-03-28 13:29:38 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen - return the length of a string
+ * _strlen - returns the length of a string
  * @s: string
  * Return: length
  */
@@ -14,5 +14,5 @@ int _strlen(char *s)
 		s++;
 	}
 
-	return (longi)
+	return (longi);
 }
