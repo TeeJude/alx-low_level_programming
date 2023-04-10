ad6a4f9254b3de7d2e70c78a48c2a5ff899c000a
Jude Tijani <teejayjude@gmail.com>
2023-04-10 16:04:56 -0700

    libmy.a, main.h

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - fill a block of memory with a specific value
+ * _memset - fills memory with a constant byte
  * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
@@ -10,7 +10,6 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
-
 
 	for (; n > 0; i++)
 	{
